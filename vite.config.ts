93a4822df4cb2d008a2b2a35eb64f9b74b0abef0
djimenezunihumboldt <djimenez@unihumboldt.edu.ve>
2025-08-08 19:06:59 -0400

    fix: configurar base para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/proyecto/', // nombre del repositorio para GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
